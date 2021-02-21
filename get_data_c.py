0fd1ea7b914f120e477cda1815076a81e24233e3
Alex <alexandra.gg150@gmail.com>
2021-02-21 13:03:07 +0100

    updated place handling with API call

--- get_data_c.py
+++ get_data_c.py
@@ -23,18 +23,11 @@ class RequestFlights():
         #self.origin_list = ['Madrid (ES)', 'Zurich (CH)']
         self.origin_list = ['Madrid', 'Zurich']
         self.origin_list_ids = []
-        self.return_trip = False
         #self.destination_list = ['London (GB)']
         self.destination_list = ['London']
         self.destination_list_ids = []
-        self.date_outbound_1 = '2021-03-01'
-        self.date_outbound_2 = '2021-03-02'
-        #self.date_outbound_1 = 'anytime'
-        #self.date_outbound_2 = None
-        self.date_range_outbound = None
-        self.date_inbound_1 = None
-        self.date_inbound_2 = None
-        self.date_range_inbound = None
+        #self.date = '2021-03-01'
+        self.date = 'anytime'
         self.num_flights = 3
         self.show_flight_info = False
         self.currency = 'EUR'
@@ -49,8 +42,7 @@ class RequestFlights():
             }
 
         columns = ['origin_sky_id', 'origin_iata_id', 'dest_sky_id', 'dest_iata_id', 'price', 'carrier', 'date']
-        self.df_outbound = pd.DataFrame(columns = columns)
-        self.df_inbound = pd.DataFrame(columns = columns)
+        self.df_flights = pd.DataFrame(columns = columns)
 
         self.headers = {
             'x-rapidapi-key': config.api_key,
@@ -58,9 +50,8 @@ class RequestFlights():
         }
 
     def __repr__(self):
-        return(f'Origins: {self.origin_list} \n Destination(s): {self.destination_list} \n Dates outbound: {self.date_outbound_1}-{self.date_outbound_2}\
-            \n Dates inbound: {self.date_inbound_1}-{self.date_inbound_2}')
-
+        return(f'Origins: {self.origin_list} \n Destination(s): {self.destination_list}')
+        
     @staticmethod
     def match_skyscanner(city_list):
         city_id = []
@@ -117,106 +108,79 @@ class RequestFlights():
         
         # format dates to datetime, and get date ranges if two dates
         # check what API allows (some restrictions if one of dates is anytime)
-        if self.date_outbound_1 != 'anytime':
-            self.date_outbound_1 = self.str_to_date(self.date_outbound_1)
-            if self.date_outbound_2 != 'anytime':
-                self.date_outbound_2 = self.str_to_date(self.date_outbound_2)
-                self.date_range_outbound = self.get_date_range(self.date_outbound_1, self.date_outbound_2)
-        else:
-            self.date_range_outbound = ['anytime']
-
-        if self.date_inbound_1 != None:
-            self.return_trip = True
-            if self.date_inbound_1 != 'anytime':
-                self.date_inbound_1 = self.str_to_date(self.date_inbound_1)
-                if self.date_inbound_2 != 'anytime ' & self.date_inbound_2 != None:
-                    self.date_inbound_2 = self.str_to_date(self.date_inbound_2)
-                    self.date_range_inbound = self.get_date_range(self.date_inbound_1, self.date_inbound_2)
-            else:
-                self.date_range_inbound = ['anytime']
+        #if self.date_outbound != 'anytime':
+            #self.date_outbound_1 = self.str_to_date(self.date_outbound_1)
+            #if self.date_outbound_2 != 'anytime':
+                #self.date_outbound_2 = self.str_to_date(self.date_outbound_2)
+                #self.date_range_outbound = self.get_date_range(self.date_outbound_1, self.date_outbound_2)
+        #else:
+            #self.date_range_outbound = ['anytime']
         
         return self
 
     def print_flights(self, airport_origin, airport_dest, carrier, date, price):
         print("-----------")
-        print(f"Outbound Journey: {airport_origin}  --> {airport_dest}")
+        print(f"Journey: {airport_origin}  --> {airport_dest}")
         print(f"Date: {date}")
         print(f"Carrier: {carrier}")      
-        print(f"Total price: {price} {self.params['currency']}")
+        print(f"Price: {price} {self.params['currency']}")
     
-    def get_flights(self, flight='outbound'):
-        if flight=='outbound':
-            date_range = self.date_range_outbound
-        elif flight=='inbound':
-            date_range = self.date_range_inbound
-
-        print('Processing flight data...')
+    def get_flights(self):
         count = 0
         request_start = time.time()
         airport_ID = {}
         carrier_ID = {}
 
-        for date_i in tqdm(date_range):
-            for origin in tqdm(self.origin_list_ids, leave=False):
-                for destination in tqdm(self.destination_list_ids, leave=False):
-                    try:
-                        count += 1
-                        if (count % 50 == 0) :
-                            pause_API(request_start)
-                            request_start = time.time() #reset time
-                        
-                        myurl = self.params['root_url'] + self.params['origin_country'] + "/" + self.params['currency'] + "/" + self.params['locale'] + "/"  + \
-                            origin + "/" + destination + "/" + date_i
-                        
-                        r = requests.request("GET", myurl, headers=self.headers)
-                        temp = json.loads(r.text)
-
-                        for places in temp["Places"]:
-                            airport_ID[places["PlaceId"]] = places["IataCode"] 
+        for origin in tqdm(self.origin_list_ids):
+            for destination in tqdm(self.destination_list_ids, leave=False):
+                try:
+                    count += 1
+                    if (count % 50 == 0) :
+                        pause_API(request_start)
+                        request_start = time.time() #reset time
                     
-                        for carrier in temp["Carriers"]:
-                            carrier_ID[carrier["CarrierId"]] = carrier["Name"]
-                        
-                        if "Quotes" in temp: 
-                            for quotes in temp["Quotes"]:
-                                # access data returned
-                                origin_outbound = quotes["OutboundLeg"]["OriginId"]                     
-                                carrier_outbound = quotes["OutboundLeg"]["CarrierIds"] #list of IDs,int
-                                dest_outbound = quotes["OutboundLeg"]["DestinationId"]
-                                price = quotes["MinPrice"]
-                                whole_date = quotes["OutboundLeg"]["DepartureDate"].split("T")
-                                date = whole_date[0]
+                    myurl = self.params['root_url'] + self.params['origin_country'] + "/" + self.params['currency'] + "/" + self.params['locale'] + "/"  + \
+                        origin + "/" + destination + "/" + self.date
+                    
+                    r = requests.request("GET", myurl, headers=self.headers)
+                    temp = json.loads(r.text)
 
-                                # match to IDs
-                                origin_iata_id = airport_ID[origin_outbound]
-                                origin_sky_id = origin_iata_id + '-sky'
-                                dest_iata_id = airport_ID[dest_outbound]
-                                dest_sky_id = dest_iata_id + '-sky'
-                                carrier_name = carrier_ID[carrier_outbound[0]]
+                    for places in temp["Places"]:
+                        airport_ID[places["PlaceId"]] = places["IataCode"] 
+                
+                    for carrier in temp["Carriers"]:
+                        carrier_ID[carrier["CarrierId"]] = carrier["Name"]
+                    
+                    if "Quotes" in temp: 
+                        for quotes in temp["Quotes"]:
+                            # access data returned
+                            origin_f = quotes["OutboundLeg"]["OriginId"]                     
+                            carrier = quotes["OutboundLeg"]["CarrierIds"] #list of IDs,int
+                            destination_f = quotes["OutboundLeg"]["DestinationId"]
+                            price = quotes["MinPrice"]
+                            date = (quotes["OutboundLeg"]["DepartureDate"].split("T"))[0]
 
-                                if flight=='outbound':
-                                    self.df_outbound = self.df_outbound.append({'origin_sky_id': origin_sky_id, 'origin_iata_id': origin_iata_id, \
-                                        'dest_sky_id': dest_sky_id, 'dest_iata_id': dest_iata_id, 'price': price, 'carrier': carrier_name, \
-                                            'date': date}, ignore_index=True)
-                                elif flight=='inbound':
-                                    self.df_inbound = self.df_inbound.append({'origin_sky_id': origin_sky_id, 'origin_iata_id': origin_iata_id, \
-                                        'dest_sky_id': dest_sky_id, 'dest_iata_id': dest_iata_id, 'price': price, 'carrier': carrier_name, \
-                                            'date': date}, ignore_index=True)
-                                
-                                if self.show_flight_info:
-                                    self.print_flights(airport_ID[origin_outbound], airport_ID[dest_outbound], carrier_ID[carrier_outbound[0]], date, price)
+                            # match to IDs
+                            origin_iata_id = airport_ID[origin_f]
+                            origin_sky_id = origin_iata_id + '-sky'
+                            dest_iata_id = airport_ID[destination_f]
+                            dest_sky_id = dest_iata_id + '-sky'
+                            carrier_name = carrier_ID[carrier[0]]
 
-                    except:
-                        continue
+                            self.df_flights = self.df_flights.append({'origin_sky_id': origin_sky_id, 'origin_iata_id': origin_iata_id, \
+                                'dest_sky_id': dest_sky_id, 'dest_iata_id': dest_iata_id, 'price': price, 'carrier': carrier_name, \
+                                    'date': date}, ignore_index=True)
+                            
+                            if self.show_flight_info:
+                                self.print_flights(airport_ID[origin_f], airport_ID[destination_f], carrier_ID[carrier[0]], date, price)
+
+                except:
+                    continue
 
         try:
-            if flight=='outbound':
-                self.df_outbound['origin_city_name'] = self.df_outbound.apply(lambda row: self.assign_city_names(row['origin_iata_id']), axis=1)
-                self.df_outbound['dest_city_name'] = self.df_outbound.apply(lambda row: self.assign_city_names(row['dest_iata_id']), axis=1)
-            elif flight=='inbound':
-                self.df_inbound['origin_city_name'] = self.df_inbound.apply(lambda row: self.assign_city_names(row['origin_iata_id']), axis=1)
-                self.df_inbound['dest_city_name'] = self.df_inbound.apply(lambda row: self.assign_city_names(row['dest_iata_id']), axis=1)
-        
+            self.df_flights['origin_city_name'] = self.df_flights.apply(lambda row: self.assign_city_names(row['origin_iata_id']), axis=1)
+            self.df_flights['dest_city_name'] = self.df_flights.apply(lambda row: self.assign_city_names(row['dest_iata_id']), axis=1)
+            
         except Exception:
             print('Origin City Not Found')
         
@@ -231,19 +195,16 @@ def main():
 
     print()
     print("Requesting flight data...")
-    print()
     Flights.get_flights()
-
-    num_out = len(Flights.df_outbound)
-    num_in = len(Flights.df_inbound)
+    
     print()
-    print(f"Number of possible outbound flights to be analyzed: {num_out}")
-    print(f"Number of possible inbound flights to be analyzed: {num_in}")
+    num = len(Flights.df_flights)
+    print(f"Number of possible flights to be evaluated: {num}")
     print()
 
     print("Done!")
 
-    print(Flights.df_outbound)
+    print(Flights.df_flights)
 
     return Flights
 
